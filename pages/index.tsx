78d49c972c73acae88931aaa95ca6f22f1dec164
Emilio Rodriguez <emiliorodriguez@emilios-mbp.lan>
2022-01-08 16:23:35 -0500

    First website approach

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Personal website building by a developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        Let's go to create amazing <br />
+        things on the internet!
+        </h1>
+
+        <p className={styles.description}>
+          Technology  •  Creativity  •  Enterpreuniship
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+          Made with 💙 by Emilio Rodriguez.
+      </footer>
+    </div>
+  )
+}
